5bd8e51f71ca203668f605ab749e1aa63abd822e
Max Andreasen <maxandr@kth.se>
2025-02-11 10:32:27 +0100

    Initial values

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import numpy , random , math
 from scipy.optimize import minimize
 import matplotlib . pyplot as plt
 
+N = 0 # Number of training samples
+C = 2 # Slack value
+B = [(0, C) for b in range(N)]
 
+start = numpy.zeros(N)
 
 print("Hello")
 input()
