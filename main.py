e62cd9fce4a12c777e4ae60610f104cd11573f87
Max Andreasen <maxandr@kth.se>
2025-02-11 11:04:16 +0100

    fixed some more stuff

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ import numpy , random , math
 from scipy.optimize import minimize
 import matplotlib . pyplot as plt
 
-N = 0 # Number of training samples
+N = 3 # Number of training samples
 C = 2 # Slack value
-B = [(0, C) for b in range(N)]
+B = [(0, C) for b in range(N)] # Boundary for alpha values
+T = [1, -1, 1] # T represents class 1 or -1
 
-start = numpy.zeros(N)
+start = numpy.zeros(N) # Starting alpha vector, initialized to zeroes. 
 
-print("Hello")
-input()
+# Check condition for running the SVM, Eq. 10
+def zerofun(alpha_vector): 
+    sum = 0
+    for i in range(len(alpha_vector)):
+        sum += (numpy.dot(alpha_vector[i], T[i]))
+
+    return sum
+
+
+constraints = {"type": "eq", "fun":zerofun}
