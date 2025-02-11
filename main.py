7d4a9d71db879962c00740fa602ff59e2e264f56
Harald Hamrin Reinhed <haraldhr@kth.se>
2025-02-11 10:57:16 +0100

    completed objective function

--- main.py
+++ main.py
@@ -2,39 +2,32 @@ import numpy , random , math
 from scipy.optimize import minimize
 import matplotlib . pyplot as plt
 
+N = 5 # temporary, nr of datapoints
+T = numpy.zeros(N) # classes (value -1 or 1)
+X = numpy.zeros((N, N)) # Vector of vectors
+C = 2 # Slack value
+B = [(0, C) for b in range(N)]
+start = numpy.zeros(N)
 
-t = [] # classes (value -1 or 1)
-X = [] # Vector of vectors
-P = [[]] # Precomputed t[i]*t[j] * kernel_linear(X[i], X[j] for efficiency.
 
 def compute_P():
-    P = [[]]
-    return numpy.array(P)
+    P = numpy.zeros((N, N))
+    for i in range(X.shape[0]):
+        for j in range(X.shape[0]):
+            P[i,j] = T[i] * T[j] * kernel_linear(X[i], X[j])
+    return P
+
+P = compute_P() # Precomputed t[i]*t[j] * kernel_linear(X[i], X[j] for efficiency.
+
 
 def kernel_linear(x_i, x_j): 
     return numpy.dot(x_i.T, x_j)
 
 def objective(alpha):
-    sum1 = 0
-    sum2 = 0
-    for i in range(len(alpha)):
-        sum2 = alpha[i]
-        for j in range(len(alpha)):
-            sum1 += alpha[i]*alpha[j]* P[i][j]
-
-    sum1 /= 2
+    sum1 = sum1 = 1/2 * numpy.dot(alpha.T, numpy.dot(P, alpha))
+    sum2 = numpy.sum(alpha)
     return sum1 - sum2
 
-    
 
 
-    pass
-
-N = 0 # Number of training samples
-C = 2 # Slack value
-B = [(0, C) for b in range(N)]
-
-start = numpy.zeros(N)
-
-print(kernel(numpy.array([1, 2]), numpy.array([3, 4])))
 input()
