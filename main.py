859c4474620b6e9e002be8f030fec4f023c70b45
Harald Hamrin Reinhed <haraldhr@kth.se>
2025-02-11 10:35:36 +0100

    added linear kernel

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import numpy , random , math
 from scipy.optimize import minimize
 import matplotlib . pyplot as plt
 
+
+def kernel_linear(x_i, x_j): 
+    return numpy.dot(x_i.T, x_j)
+
 N = 0 # Number of training samples
 C = 2 # Slack value
 B = [(0, C) for b in range(N)]
 
 start = numpy.zeros(N)
 
-print("Hello")
+print(kernel(numpy.array([1, 2]), numpy.array([3, 4])))
 input()
